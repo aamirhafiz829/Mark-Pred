dae1bf691872928c2524c1c87a4c72d08d324c4e
Aamir <aamir829hafiz@gmail.com>
2024-02-23 00:34:30 +0000

    started caching

--- helloworld/ml.py
+++ helloworld/ml.py
@@ -5,11 +5,15 @@ from sklearn.metrics import precision_score,confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from simulations import simulate_trading
+
 def analyzeRF(df,tickerSymbol):
+    df,new_predictors = preprocessing(df, tickerSymbol)
+    return trainRF(df, new_predictors,tickerSymbol)
+def preprocessing(df,tickerSymbol):
     df.index = pd.to_datetime(df.index)
     df["Tomorrow"] = df["Close"].shift(-1)
     df["Target"] = (df["Tomorrow"] > df["Close"]).astype(int)
-    model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+    #model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
     horizons = [5,60,250]
     new_predictors = ["Volume"]
     for horizon in horizons:
@@ -27,11 +31,16 @@ def analyzeRF(df,tickerSymbol):
         
         new_predictors+= [ratio_column, trend_column, vol_ratio_column]
     df = df.dropna(subset=df.columns[df.columns != "Tomorrow"])
-    
+    #return df
+
+
     train = df.iloc[:-100]
     test = df.iloc[-100:]
     print("train",train.head())
     print("test",test.head())
+    return df,new_predictors
+def trainRF(df,new_predictors,tickerSymbol):
+    model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
     predictions = backtest(df, model, new_predictors)
     multiPredictions = multiPredictBacktest(df,model,new_predictors)
     differences = predictions["Predictions"] - predictions["Target"]
@@ -43,8 +52,10 @@ def analyzeRF(df,tickerSymbol):
     output = "Balances per share: \n"
     for i in range(len(balance)):
         output += "$ "+str(round(balance[i], 3)) + " with " + str(transactions[i]) + " transactions\n"
-    return (confusion_matrix(predictions["Target"], predictions["Predictions"]), output)
     
+    return (confusion_matrix(predictions["Target"], predictions["Predictions"]), output)
+#def cacheRFModel(model, tickerSymbol):
+
 def predictRF(train, test, predictors, model):
     model.fit(train[predictors], train["Target"])
     preds = model.predict_proba(test[predictors])[:,1]
